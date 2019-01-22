a0650c9941589a128572bee1aa5127419e273b6a
Fergus Lemon <fergus.lemon@gmail.com>
2019-01-22 20:54:54 +0000

    Selection sort in Javascript.

---
+++ selectionSort.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+'use strict';
+
+function findSmallest (arr) {
+  let smallest = arr[0];
+  let smallestIndex = 0;
+  let len = arr.length;
+
+  for(var i = 1; i < len; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i];
+      smallestIndex = i;
+    }
+  }
+    return smallestIndex;
+}
+
+function selectionSort (arr) {
+  let newArr = [];
+  let len = arr.length;
+  for (var i = 0; i < len; i++) {
+    let smallest = findSmallest(arr);
+    newArr.push(arr.splice(smallest, 1));
+  }
+  return newArr.flat();
+}
