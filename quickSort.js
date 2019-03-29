36865da525022e446cdd9fb6df02a4f0d025da2b
FergusLemon <fergus.lemon@gmail.com>
2019-03-29 12:05:50 +0000

    Update quickSort.js
    Use a random pivot so that there is a better chance of achieving the average case for the algorithm which would run in O(n log n).

--- quickSort.js
+++ quickSort.js
@@ -2,14 +2,19 @@ quickSort = (arr) => {
     if (arr.length < 2) {
         return arr;
     } else {
-        let pivot = arr[0];
-        let slicedArray = arr.slice(1);
-        let smallerElements = slicedArray.filter((element) => {
-            return element <= pivot;
-        });
-        let largerElements = slicedArray.filter((element) => {
-            return element > pivot;
-        });
-        return quickSort(smallerElements).concat([pivot]).concat(quickSort(largerElements));
+        let min = 0;
+        let max = arr.length;
+        let pivotPoint = Math.floor(Math.random() * (max - min)) + min;
+        let pivot = arr[pivotPoint];
+        let smaller = []; 
+        let larger = [];
+        for (let element of arr) {
+            if (element <= pivot && arr.indexOf(element) !== pivotPoint) {
+                smaller.push(element);
+            } else if (element > pivot) {
+                larger.push(element);
+            }
+        };
+        return quickSort(smaller).concat([pivot]).concat(quickSort(larger));
     }
 };
